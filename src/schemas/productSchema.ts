0a39b53e003f0903c86fded818ff7234c35bef90
Reihan Oktavio <reihanoktavio@Reihans-MacBook-Air.local>
2024-12-07 05:58:47 +0700

    tugas typescript

---
+++ src/schemas/productSchema.ts
@@ -0,0 +1,9 @@
+export const productSchema = {
+    id: "number",
+    name: "string",
+    description: "string",
+    price: "number",
+    category: "string",
+    stock: "number"
+  };
+  
---
+++ src/schemas/userSchema.ts
@@ -0,0 +1,8 @@
+export const userSchema = {
+    id: "number",
+    name: "string",
+    email: "string",
+    password: "string",
+    address: "string"
+  };
+  
